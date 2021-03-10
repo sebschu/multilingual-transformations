7416cb3a98692e4a00996e98139b5082072dc806
Sebastian Schuster <sebschu@stanford.edu>
2021-03-10 10:51:49 -0500

    prepare finetuning script for greene cluster

--- scripts/finetune_mt5_mccoy.sh
+++ scripts/finetune_mt5_mccoy.sh
@@ -1,11 +1,25 @@
+#SBATCH --gres=gpu:1
+#!/bin/bash
+
+#SBATCH --job-name=MT-base-finetune
+#SBATCH --nodes=1 
+#SBATCH --cpus-per-task=1 
+#SBATCH --mem=20GB 
+#SBATCH --time=10:00:00 
+
+
+
+conda activate py38
+
 python ../models/run_seq2seq.py \
-    --model_name_or_path mt5-base \
-    --do_train \
+    --model_name_or_path 'google/mt5-base' \
+    --do_train \--gres=gpu:1
+
     --do_eval \
     --task translation_src_to_tgt \
     --train_file ../data/mccoy2020/question_main.train.json \
     --validation_file ../data/mccoy2020/question_main.dev.json \
-    --output_dir ../output/ \
+    --output_dir $SCRATCH/mt5-mccoy-finetuning/	 \
     --per_device_train_batch_size=4 \
     --per_device_eval_batch_size=4 \
     --overwrite_output_dir \
