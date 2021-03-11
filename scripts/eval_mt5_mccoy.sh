a1e3c4ef70a06f9ced5d51a3bed76066f8953b35
Sebastian Schuster <schuster@nyu.edu>
2021-03-11 10:37:15 -0500

    update fine-tuning and eval scripts

--- scripts/eval_mt5_mccoy.sh
+++ scripts/eval_mt5_mccoy.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-#SBATCH --job-name=MT5-base-finetune
+#SBATCH --job-name=MT-base-finetune
 #SBATCH --nodes=1 
 #SBATCH --cpus-per-task=1 
 #SBATCH --mem=20GB 
 #SBATCH --time=10:00:00 
 #SBATCH --gres=gpu:v100:1
 
-
 source /share/apps/anaconda3/2020.07/bin/activate py38
 
 CUDA_LAUNCH_BLOCKING=1 python ../models/run_seq2seq.py \
-    --model_name_or_path 'google/mt5-base' \
-    --do_train \
+    --model_name_or_path /scratch/ss9536/mt5-mccoy-finetuning/ \
     --do_eval \
     --task translation_src_to_tgt \
-    --train_file ../data/mccoy2020/question.train.json \
-    --validation_file ../data/mccoy2020/question.dev.json \
+    --train_file ../data/mccoy2020/question_main.train.json \
+    --validation_file ../data/mccoy2020/question_main.dev.json \
     --output_dir $SCRATCH/mt5-mccoy-finetuning/	 \
     --per_device_train_batch_size=4 \
     --per_device_eval_batch_size=4 \
