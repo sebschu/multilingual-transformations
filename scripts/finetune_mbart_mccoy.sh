1d7b7577044d435c70c504bb3528370cc48548fe
Aaron Mueller <am12057@log-3.nyu.cluster>
2021-03-18 12:21:03 -0400

    standardize mbart script for NYU Greene

--- scripts/finetune_mbart_mccoy.sh
+++ scripts/finetune_mbart_mccoy.sh
@@ -1,15 +1,14 @@
-#SBATCH --gres=gpu:1
 #!/bin/bash
 
-#SBATCH --job-name=MT-base-finetune
+#SBATCH --job-name=MBart-large-finetune
 #SBATCH --nodes=1 
 #SBATCH --cpus-per-task=1 
 #SBATCH --mem=20GB 
 #SBATCH --time=10:00:00 
+#SBATCH --gres=gpu:v100:1
 
 
-
-conda activate torch
+source $HOME/miniconda3/bin/activate py38
 
 python ../models/run_seq2seq.py \
     --model_name_or_path 'facebook/mbart-large-cc25' \
@@ -17,11 +16,11 @@ python ../models/run_seq2seq.py \
     --do_eval \
     --task translation_src_to_tgt \
 	--source_lang en_XX \
-	--target_lang ro_RO \
+	--target_lang en_XX \
 	--use_fast_tokenizer False \
     --train_file ../data/mccoy2020/question_main.train.json \
     --validation_file ../data/mccoy2020/question_main.dev.json \
-    --output_dir mbart_output	 \
+    --output_dir $SCRATCH/mbart-mccoy-finetuning	 \
     --per_device_train_batch_size=4 \
     --per_device_eval_batch_size=4 \
     --overwrite_output_dir \
