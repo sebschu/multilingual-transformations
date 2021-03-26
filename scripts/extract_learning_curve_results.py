1ab073228c6734a20562d29176f7f21193803555
Sebastian Schuster <ss9536@nyu.edu>
2021-03-26 10:44:20 -0400

    add learning curve script

---
+++ scripts/extract_learning_curve_results.py
@@ -0,0 +1,20 @@
+import glob
+import sys
+import re
+
+wildcard_path = sys.argv[1]
+
+print("iteration,exact_match,first_word_accuracy")
+
+for path in glob.glob(wildcard_path):
+  it_res = re.match(".*checkpoint-([0-9]+)[/].*", path)
+  it = it_res.group(1)
+  with open(path, "r") as res_file:
+    results = []
+    for line in res_file:
+      parts = line.strip().split(":")
+      results.append(parts[1].strip())
+    res_str = ",".join(results)
+    print(f"{it},{res_str}")
+
+    
