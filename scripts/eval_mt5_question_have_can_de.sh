e7996214ab196a2b97e9f7f1fee248080c8b2ab1
aaronmueller <ammaaronmueller@gmail.com>
2021-04-22 17:37:21 -0400

    fixed mt5 eval script for german have_can data

--- scripts/eval_mt5_question_have_can_de.sh
+++ scripts/eval_mt5_question_have_can_de.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=1 
 #SBATCH --mem=20GB 
 #SBATCH --time=40:00:00 
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:v100:1
 
 
 
@@ -14,14 +14,14 @@ singularity exec --nv --overlay $SCRATCH/overlay-50G-10M.ext3:ro /scratch/work/p
 source /ext3/env.sh
 conda activate py38
 
-python ../models/run_seq2seq.py \
+CUDA_LAUNCH_BLOCKING=1 python ../models/run_seq2seq.py \
     --model_name_or_path 'google/mt5-base' \
 	--do_eval \
 	--do_learning_curve \
     --task translation_src_to_tgt \
     --train_file ../data/question_have-can_de/question_have_can.en-de.train.json \
-    --validation_file ../data/question_have-can_de/question_have_can.de.gen_rc_o.json \
-    --output_dir /scratch/am12057/mt5-mccoy-finetuning-question-have-can-en-de/  \
+    --validation_file ../data/question_have-can_de/question_have_can.de.gen_rc_s.json \
+    --output_dir $SCRATCH/mt5-mccoy-finetuning-question-have-can-en-de/  \
     --per_device_train_batch_size=4 \
     --per_device_eval_batch_size=16 \
     --overwrite_output_dir \
