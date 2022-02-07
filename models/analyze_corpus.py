8c666a3690bd6f4724cd012a340abec4e9a9edd7
aaronmueller <ammaaronmueller@gmail.com>
2022-02-07 16:45:48 -0500

    present top-k sentences in

--- models/analyze_corpus.py
+++ models/analyze_corpus.py
@@ -4,16 +4,25 @@ import argparse
 from datasets import load_dataset
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser()
-parser.add_argument("num_examples", type=int, help="Number of examples to analyze.")
-parser.add_argument("-l", "--language", type=str, default="en",
-                    help="Language on which we perform the corpus analysis.")
-parser.add_argument("-m", "--metric", type=str, default="jaccard_sim",
-                    help="Similarity metric for comparing sentences.")
-parser.add_argument("-i", "--ignore-case", action="store_true", default=False,
-                    help="Whether to ignore casing in input text.")
-args = parser.parse_args()
+# UTILITY FUNCTIONS
+def reverse_insort(a, x, lo=0, hi=None):
+    """Insert item x in list a, and keep it reverse-sorted.
 
+    If x is already in a, insert it to the right of the rightmost x.
+
+    Optional args lo and hi bound the slice of a to be searched.
+    """
+    if lo < 0:
+        raise ValueError("lo must be non-negative")
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo+hi)//2
+        if x[1] > a[mid][1]: hi = mid
+        else: lo = mid+1
+    a.insert(lo, x)
+
+# SIMILARITY METRICS
 # calculate proportion of word set overlap across two sentences
 # TODO: take into account words that appear multiple times (use Counter?)
 def jaccard_sim(sent_1, sent_2):
@@ -27,51 +36,75 @@ def jaccard_sim(sent_1, sent_2):
 def dependency_sim(sent_1, sent_2):
     pass
 
+
 METRIC_FUNCTIONS = {
     "jaccard_sim": jaccard_sim,
     "dependency_sim": dependency_sim
 }
 
-en_mc4 = load_dataset("mc4", args.language, split="train", streaming=True)
-# make it iterable using `next`
-en_mc4 = iter(en_mc4)
 
-# for this to work, you'll have to run `python -m spacy download en_core_web_sm
-if args.language == "en":
-    nlp = spacy.load("en_core_web_sm")
-else:
-    raise NotImplementedError(f"Language \"{args.language}\" not supported.")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("num_examples", type=int, help="Number of examples to analyze.")
+    parser.add_argument("-l", "--language", type=str, default="en",
+                        help="Language on which we perform the corpus analysis.")
+    parser.add_argument("-m", "--metric", type=str, default="jaccard_sim",
+                        help="Similarity metric for comparing sentences.")
+    parser.add_argument("-i", "--ignore-case", action="store_true", default=False,
+                        help="Whether to ignore casing in input text.")
+    parser.add_argument("-n", "--num_sents", type=int, default=50,
+                        help="Display the top <num_sents> sentences by similarity.")
+    args = parser.parse_args()
 
-skipped = 0
-max_sim = 0
-max_sim_sents = [None, None]
 
-# iterate through mC4 documents looking for disambiguating sentence pairs
-for _ in tqdm(range(args.num_examples)):
-    example = next(en_mc4)["text"]
-    if args.ignore_case:
-        example = example.lower()
-    doc = nlp(example)
-    sentences = list(doc.sents)
-    # skip document if only 1 sentence is present
-    if len(sentences) < 2:
-        skipped += 1
-        continue
-    # compare adjacent sentences in the training example
-    prev_sent = None
-    for idx in range(1, len(sentences)):
-        if not prev_sent:
-            sent_1 = [token.text for token in sentences[idx-1]]
-        else:
-            sent_1 = prev_sent
-        # sentences are stored as lists of tokens (incl. punctuation)
-        sent_2 = [token.text for token in sentences[idx]]
-        prev_sent = sent_2
+    en_mc4 = load_dataset("mc4", args.language, split="train", streaming=True)
+    # make it iterable using `next`
+    en_mc4 = iter(en_mc4)
 
-        sent_sim = METRIC_FUNCTIONS[args.metric](sent_1, sent_2)
-        if sent_sim > max_sim:
-            max_sim = sent_sim
-        max_sim_sents = [sent_1, sent_2]
+    if args.language == "en":
+        # for this to work, you'll have to run `python -m spacy download en_core_web_sm`
+        nlp = spacy.load("en_core_web_sm")
+    else:
+        raise NotImplementedError(f"Language \"{args.language}\" not supported.")
 
-print(f"maximally similar sentences:\n{max_sim_sents[0]}\n-------\n{max_sim_sents[1]}\n")
-print(f"# skipped documents: {skipped}")
+    skipped = 0
+    max_sim_sents = []  # format of elements: ([sent_1, sent_2], similarity_score)
+    # iterate through mC4 documents looking for disambiguating sentence pairs
+    for _ in tqdm(range(args.num_examples)):
+        example = next(en_mc4)["text"]
+        if args.ignore_case:
+            example = example.lower()
+        doc = nlp(example)
+        sentences = list(doc.sents)
+        # skip document if only 1 sentence is present
+        if len(sentences) < 2:
+            skipped += 1
+            continue
+
+        # compare adjacent sentences in the training example
+        prev_sent = None
+        for idx in range(1, len(sentences)):
+            if not prev_sent:
+                sent_1 = [token.text for token in sentences[idx-1]]
+            else:
+                sent_1 = prev_sent
+            # sentences are stored as lists of tokens (incl. punctuation)
+            sent_2 = [token.text for token in sentences[idx]]
+            prev_sent = sent_2
+
+            sent_sim = METRIC_FUNCTIONS[args.metric](sent_1, sent_2)
+            sent_tuple = ([sent_1, sent_2], sent_sim)
+            if len(max_sim_sents) < args.num_sents:
+                reverse_insort(max_sim_sents, sent_tuple)
+            elif sent_sim > max_sim_sents[-1][1]:
+                reverse_insort(max_sim_sents, sent_tuple)
+                max_sim_sents.pop()
+
+    max_sim_sents = reversed(max_sim_sents)
+    for sent_tuple in max_sim_sents:
+        sent_1, sent_2 = sent_tuple[0]
+        sent_sim = sent_tuple[1]
+        print(f"{args.metric}: {sent_sim}")
+        print(f"\n{' '.join(sent_1)}\n---\n{' '.join(sent_2)}\n")
+        print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
+    print(f"# skipped documents: {skipped}")
